fec35f27a4765e8859fb637672c064f40296f6c3
pinkky166 <pinky.a@zend.com>
2014-05-28 17:47:11 +0530

    knk

---
+++ newfile.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is a sample script from Paul Conrad's Independent Study
+// This script will call the function named foo showing case insensitivity.
+function foo($my_string) {
+	echo "$"."my_string = $my_string<br>";
+}
+foo("foo is foo");
+Foo("Foo is foo");
+FoO("FoO is foo");
+FOO("FOO is foo");
+?>
